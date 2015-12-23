86869bb64ff18e195ef61ee212d2709092f84e1a
Mathias Klippinge <mathias.klippinge@gmail.com>
2015-12-23 13:43:52 +0100

    Found the basement for day01

--- day01/parser.rb
+++ day01/parser.rb
@@ -2,21 +2,40 @@ module Day01
   class Parser
     def initialize(input)
       @input = input
+      @entered_basement_at = nil
     end
 
     def to_s
-      "The floor is: #{follow_instructions.to_s}"
+      "The final floor is: #{follow_instructions}\nThe basement is first entered at: #{@entered_basement_at}"
+    end
+
+    def find_basement
+      if @entered_basement_at.nil?
+        follow_instructions
+      end
+
+      if @entered_basement_at.nil?
+        'Never'
+      else
+        @entered_basement_at
+      end
     end
 
     def follow_instructions
-      @input.split(//).inject(0) do |floor, instruction|
+      @input.split(//).each_with_index.inject(0) do |floor, instruction_index|
+        instruction = instruction_index[0]
+        index = instruction_index[1]
         if instruction == '('
           floor += 1
         elsif instruction == ')'
           floor -= 1
-        else
-          floor = floor
         end
+
+        if floor < 0 && @entered_basement_at.nil?
+          @entered_basement_at = index + 1
+        end
+
+        floor
       end
     end
   end
--- test/day01/parser_test.rb
+++ test/day01/parser_test.rb
@@ -2,48 +2,62 @@ require 'minitest/autorun'
 require './day01/day01'
 
 describe Day01::Parser do
-  it '(()) result in floor 0' do
-    subject = Day01::Parser.new('(())')
-    subject.follow_instructions.must_equal 0
+  describe '#follow_instructions' do
+    it '(()) result in floor 0' do
+      subject = Day01::Parser.new('(())')
+      subject.follow_instructions.must_equal 0
+    end
+
+    it '()() result in floor 0' do
+      subject = Day01::Parser.new('()()')
+      subject.follow_instructions.must_equal 0
+    end
+
+    it '((( result in floor 3' do
+      subject = Day01::Parser.new('(((')
+      subject.follow_instructions.must_equal 3
+    end
+
+    it '(()(()( result in floor 3' do
+      subject = Day01::Parser.new('(()(()(')
+      subject.follow_instructions.must_equal 3
+    end
+
+    it '))((((( result in floor 3' do
+      subject = Day01::Parser.new('))(((((')
+      subject.follow_instructions.must_equal 3
+    end
+
+    it '()) result in floor -1' do
+      subject = Day01::Parser.new('())')
+      subject.follow_instructions.must_equal -1
+    end
+
+    it '))( result in floor -1' do
+      subject = Day01::Parser.new('))(')
+      subject.follow_instructions.must_equal -1
+    end
+
+    it '))) result in floor -3' do
+      subject = Day01::Parser.new(')))')
+      subject.follow_instructions.must_equal -3
+    end
+
+    it ')())()) result in floor -1' do
+      subject = Day01::Parser.new(')())())')
+      subject.follow_instructions.must_equal -3
+    end
   end
 
-  it '()() result in floor 0' do
-    subject = Day01::Parser.new('()()')
-    subject.follow_instructions.must_equal 0
-  end
+  describe '#find_basement' do
+    it ') result in position 1' do
+      subject = Day01::Parser.new(')')
+      subject.find_basement.must_equal 1
+    end
 
-  it '((( result in floor 3' do
-    subject = Day01::Parser.new('(((')
-    subject.follow_instructions.must_equal 3
-  end
-
-  it '(()(()( result in floor 3' do
-    subject = Day01::Parser.new('(()(()(')
-    subject.follow_instructions.must_equal 3
-  end
-
-  it '))((((( result in floor 3' do
-    subject = Day01::Parser.new('))(((((')
-    subject.follow_instructions.must_equal 3
-  end
-
-  it '()) result in floor -1' do
-    subject = Day01::Parser.new('())')
-    subject.follow_instructions.must_equal -1
-  end
-
-  it '))( result in floor -1' do
-    subject = Day01::Parser.new('))(')
-    subject.follow_instructions.must_equal -1
-  end
-
-  it '))) result in floor -3' do
-    subject = Day01::Parser.new(')))')
-    subject.follow_instructions.must_equal -3
-  end
-
-  it ')())()) result in floor -1' do
-    subject = Day01::Parser.new(')())())')
-    subject.follow_instructions.must_equal -3
+    it '()()) result in position 5' do
+      subject = Day01::Parser.new('()())')
+      subject.find_basement.must_equal 5
+    end
   end
 end
