d18a6ca288377c0e96029a2f715f46086bb22c12
Mathias Klippinge <mathias.klippinge@gmail.com>
2015-12-25 20:43:48 +0100

    Performance

--- day04/parser.rb
+++ day04/parser.rb
@@ -5,21 +5,16 @@ module Day04
   class Parser
     def initialize(input)
       @input=input.strip
-      @i=0
-      @md5 = Digest::MD5.new
-      @md5.hexdigest
-      @md5.reset
     end
 
     def to_s
-      result=''
-      puts Benchmark.measure { result = "Result: #{find_md5}\n" }
-      result
+      puts Benchmark.measure { puts "Result, part1: #{find_md5('00000')}\n" }
+      puts Benchmark.measure { puts "Result, part2: #{find_md5('000000')}\n" }
     end
 
-    def find_md5
-      until (md5(@i).slice(0,5) == '00000') do
-        @md5.reset
+    def find_md5(prefix)
+      @i = 0
+      until md5(@i).start_with?(prefix) do
         @i += 1
       end
       @i
@@ -28,7 +23,7 @@ module Day04
     private
 
     def md5(i)
-      @md5.update("#{@input}#{i}").to_s
+      Digest::MD5.hexdigest("#{@input}#{i}")
     end
   end
 end
