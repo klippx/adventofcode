0fa74c5b3ab1b8d22786f360b0f1bd271e8a36a9
Mathias Klippinge <mathias.klippinge@gmail.com>
2015-12-25 19:17:38 +0100

    faster md5?

--- day04/parser.rb
+++ day04/parser.rb
@@ -1,27 +1,34 @@
 require 'digest'
+require 'benchmark'
 
 module Day04
   class Parser
     def initialize(input)
       @input=input.strip
+      @i=0
+      @md5 = Digest::MD5.new
+      @md5.hexdigest
+      @md5.reset
     end
 
     def to_s
-      "Result: #{find_md5}\n"
+      result=''
+      puts Benchmark.measure { result = "Result: #{find_md5}\n" }
+      result
     end
 
     def find_md5
-      i=0
-      until (md5(i).slice(0,5) == '00000') do
-        i += 1
+      until (md5(@i).slice(0,5) == '00000') do
+        @md5.reset
+        @i += 1
       end
-      i
+      @i
     end
 
     private
 
     def md5(i)
-      Digest::MD5.hexdigest "#{@input}#{i}"
+      @md5.update("#{@input}#{i}").to_s
     end
   end
 end
