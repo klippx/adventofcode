502d4ee94a3480b5792344728368a65e0600262b
Mathias Klippinge <mathias.klippinge@gmail.com>
2015-12-25 20:44:47 +0100

    Fix spec

--- test/day04/parser_test.rb
+++ test/day04/parser_test.rb
@@ -4,11 +4,11 @@ require './day04/day04'
 describe Day04::Parser do
   describe '#find_md5' do
     it 'for input abcdef it returns 609043' do
-      Day04::Parser.new('abcdef').find_md5.must_equal 609043
+      Day04::Parser.new('abcdef').find_md5('00000').must_equal 609043
     end
 
     it 'for input pqrstuv it returns 1048970' do
-      Day04::Parser.new('pqrstuv').find_md5.must_equal 1048970
+      Day04::Parser.new('pqrstuv').find_md5('00000').must_equal 1048970
     end
   end
 end
