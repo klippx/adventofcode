0c7b9b49266637d6d0a04b7d70a60b68efac7690
Mathias Klippinge <mathias.klippinge@gmail.com>
2015-12-26 20:03:36 +0100

    parse input

--- day06/parser.rb
+++ day06/parser.rb
@@ -2,13 +2,43 @@ module Day06
   class Parser
     def initialize(input)
       @input=input
+      @grid={}
     end
 
     def to_s
-      "Result: #{some_method}\n"
+      "Result -- part1: #{lights_count}\n"
     end
 
-    def some_method
+    def lights_count
+      @input.split("\n").map do |instruction|
+        parsed_instruction = parse_instruction(instruction)
+      end
+    end
+
+    private
+
+    def parse_instruction(instruction)
+      method = instruction.start_with?('turn on') && :turn_on ||
+               instruction.start_with?('turn off') && :turn_off ||
+               instruction.start_with?('toggle') && :toggle or
+               fail "Unknown instruction; #{instruction}"
+
+      start_coord = instruction.match(/(\d+,\d+)/)[1] or
+                    fail "Unknown start coordinates; #{instruction}"
+      end_coord =   instruction.match(/\d+,\d+[^\d]+(\d+,\d+)/)[1] or
+                    fail "Unknown end coordinates; #{instruction}"
+
+      {
+        method: method,
+        start: {
+          x: start_coord.match(/(\d+)/)[1].to_i,
+          y: start_coord.match(/\d+,(\d+)/)[1].to_i,
+        },
+        end: {
+          x: end_coord.match(/(\d+)/)[1].to_i,
+          y: end_coord.match(/\d+,(\d+)/)[1].to_i,
+        },
+      }
     end
   end
 end
