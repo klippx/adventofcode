5442d84c44a3959a4cb074020766c53897d0ec63
Mathias Klippinge <mathias.klippinge@gmail.com>
2015-12-26 17:46:33 +0100

    day05 part2, wrong answer

--- day05/nice_checker2.rb
+++ day05/nice_checker2.rb
@@ -5,7 +5,24 @@ module Day05
     end
 
     def is_nice?
-      true
+       at_least_one_repeating_letter_with_one_letter_between? && has_recurring_substrings_of_size_two?|| false
+    end
+
+    # It contains a pair of any two letters that appears at least twice in the string without overlapping,
+    # like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+    def has_recurring_substrings_of_size_two?
+      pairs = (
+        @string.split(//).each_slice(2).map { |pair| pair } +
+        @string[(1..-1)].split(//).each_slice(2).map { |pair| pair }
+      ).select { |pair| pair.size > 1 }
+
+      pairs != pairs.uniq
+    end
+
+    # It contains at least one letter which repeats with exactly one letter between them,
+    # like xyx, abcdefeghi (efe), or even aaa.
+    def at_least_one_repeating_letter_with_one_letter_between?
+      @string =~ /(\w)\w\1/ && true
     end
   end
 end
--- test/day05/nice_checker2_test.rb
+++ test/day05/nice_checker2_test.rb
@@ -2,6 +2,40 @@ require 'minitest/autorun'
 require './day05/day05'
 
 describe Day05::NiceChecker2 do
+  describe '#at_least_one_repeating_letter_with_one_letter_between?' do
+    it 'xyx is true' do
+      subject = Day05::NiceChecker2.new('xyx')
+      subject.at_least_one_repeating_letter_with_one_letter_between?.must_equal true
+    end
+
+    it 'abcdefeghi is true' do
+      subject = Day05::NiceChecker2.new('abcdefeghi')
+      subject.at_least_one_repeating_letter_with_one_letter_between?.must_equal true
+    end
+
+    it 'aaa is true' do
+      subject = Day05::NiceChecker2.new('aaa')
+      subject.at_least_one_repeating_letter_with_one_letter_between?.must_equal true
+    end
+  end
+
+  describe '#has_recurring_substrings_of_size_two?' do
+    it 'xyxy is true' do
+      subject = Day05::NiceChecker2.new('xyxy')
+      subject.has_recurring_substrings_of_size_two?.must_equal true
+    end
+
+    it 'aabcdefgaa is true' do
+      subject = Day05::NiceChecker2.new('aabcdefgaa')
+      subject.has_recurring_substrings_of_size_two?.must_equal true
+    end
+
+    it 'aaa is false' do
+      subject = Day05::NiceChecker2.new('aaa')
+      subject.has_recurring_substrings_of_size_two?.must_equal false
+    end
+  end
+
   describe '#is_nice?' do
     it 'qjhvhtzxzqqjkmpb is nice' do
       subject = Day05::NiceChecker2.new('qjhvhtzxzqqjkmpb')
