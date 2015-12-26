ec06021fdd6c79f78a06530470e27bcce1bcb5b7
Mathias Klippinge <mathias.klippinge@gmail.com>
2015-12-26 22:13:14 +0100

    Day06 done -- runs in about 1min

--- day06/parser.rb
+++ day06/parser.rb
@@ -8,17 +8,19 @@ module Day06
     end
 
     def to_s
-      "Result -- part1: #{lights_count}\n"
+      "Result -- part1: #{lights_count}\n" +
+      "Result -- part2: #{brigthness_count}\n"
     end
 
     def lights_count
+      @grid={}
       bench = { turn_on: [], turn_off: [], toggle: [], parse_instruction: [] }
       parsed_instruction, start_pos, end_pos = nil, nil, nil
       t1=Time.now
       @input.split("\n").each_with_index do |instruction, i|
         bench[:parse_instruction] << Benchmark.realtime { parsed_instruction, start_pos, end_pos = parse_instruction(instruction) }
         bench[parsed_instruction] << Benchmark.realtime { send(parsed_instruction, start_pos, end_pos) }
-        if i > 0 && i%25 == 0
+        if i > 0 && i%50 == 0
           t2=Time.now
           elapsed_time = t2-t1
           velocity = i/elapsed_time
@@ -35,6 +37,34 @@ module Day06
       end
 
       @grid.keys.count
+    end
+
+    def brigthness_count
+      @grid={}
+      bench = { turn_on_2: [], turn_off_2: [], toggle_2: [], parse_instruction: [] }
+      parsed_instruction, start_pos, end_pos = nil, nil, nil
+      t1=Time.now
+      @input.split("\n").each_with_index do |instruction, i|
+        bench[:parse_instruction] << Benchmark.realtime { parsed_instruction, start_pos, end_pos = parse_instruction(instruction) }
+        parsed_instruction = "#{parsed_instruction}_2".to_sym
+        bench[parsed_instruction] << Benchmark.realtime { send(parsed_instruction, start_pos, end_pos) }
+        if i > 0 && i%50 == 0
+          t2=Time.now
+          elapsed_time = t2-t1
+          velocity = i/elapsed_time
+          distance_left = 300-i
+          printf "\nBenchmark report; Progress: %.2f%%, (elapsed time=%.2fs), (time left=%.2fs)\n", i/3.0, elapsed_time, distance_left/velocity
+          bench.keys.each do |key|
+            next unless bench[key].size > 0
+            total = bench[key].inject(:+)
+            count = bench[key].size
+            avg = bench[key].inject(:+)
+            puts key.to_s.ljust(20) + sprintf("Time spent in method: %.2fs, relative: %.2f%%", total, total/elapsed_time*100)
+          end
+        end
+      end
+
+      @grid.map { |_key, value| value }.inject(:+)
     end
 
     private
@@ -93,6 +123,42 @@ module Day06
 
     def key(x,y)
       "#{x}x#{y}y"
+    end
+
+    def turn_on_2(start_pos, end_pos)
+      (start_pos[:x]..end_pos[:x]).each do |x|
+        (start_pos[:y]..end_pos[:y]).each do |y|
+          if @grid.key? key(x,y)
+            @grid[key(x,y)] += 1
+          else
+            @grid[key(x,y)] = 1
+          end
+        end
+      end
+    end
+
+    def turn_off_2(start_pos, end_pos)
+      (start_pos[:x]..end_pos[:x]).each do |x|
+        (start_pos[:y]..end_pos[:y]).each do |y|
+          if @grid.key?(key(x,y)) && @grid[key(x,y)] > 0
+            @grid[key(x,y)] -= 1
+          elsif @grid.key?(key(x,y)) && @grid[key(x,y)] == 0
+            @grid.delete key(x,y)
+          end
+        end
+      end
+    end
+
+    def toggle_2(start_pos, end_pos)
+      (start_pos[:x]..end_pos[:x]).each do |x|
+        (start_pos[:y]..end_pos[:y]).each do |y|
+          if @grid.key? key(x,y)
+            @grid[key(x,y)] += 2
+          else
+            @grid[key(x,y)] = 2
+          end
+        end
+      end
     end
   end
 end
