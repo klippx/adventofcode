2a10c5b610c18b351a64a1323738ca50b7e7790a
Mathias Klippinge <mathias.klippinge@gmail.com>
2015-12-28 13:42:44 +0100

    Added Gate

--- day07/parser.rb
+++ day07/parser.rb
@@ -11,4 +11,51 @@ module Day07
     def some_method
     end
   end
+
+  class Gate
+    def initialize(input_one, operator, input_two=nil)
+      @operator=operator.downcase.to_sym
+      @input_one=input_one
+      @input_two=input_two
+    end
+
+    def output
+      self.send(@operator)
+    end
+
+    def and
+      @input_one & @input_two
+    end
+
+    def or
+      @input_one | @input_two
+    end
+
+    def rshift
+      @input_one >> @input_two
+    end
+
+    def lshift
+      @input_one << @input_two
+    end
+
+    def not
+      @input_one
+        .to_s(2)
+        .rjust(16, '0')
+        .split(//)
+        .map { |d| d == '0' ? '1' : '0' }
+        .join('')
+        .to_i(2)
+    end
+  end
+
+  # Gate:     source=Gate.new(...).output
+  # Constant: source=123
+  class Wire
+    def initialize(name)
+      @name=name
+      @source=source
+    end
+  end
 end
--- test/day07/parser_test.rb
+++ test/day07/parser_test.rb
@@ -7,3 +7,31 @@ describe Day07::Parser do
     end
   end
 end
+
+describe Day07::Gate do
+  describe '#output' do
+    it '123 AND 456 gives 72' do
+      Day07::Gate.new(123, 'AND', 456).output.must_equal 72
+    end
+
+    it '123 OR 456 gives 507' do
+      Day07::Gate.new(123, 'OR', 456).output.must_equal 507
+    end
+
+    it '123 LSHIFT 2 gives 492' do
+      Day07::Gate.new(123, 'LSHIFT', 2).output.must_equal 492
+    end
+
+    it '456 RSHIFT 2 gives 114' do
+      Day07::Gate.new(456, 'RSHIFT', 2).output.must_equal 114
+    end
+
+    it 'NOT 123 gives 65412' do
+      Day07::Gate.new(123, 'NOT').output.must_equal 65412
+    end
+
+    it 'NOT 456 gives 65079' do
+      Day07::Gate.new(456, 'NOT').output.must_equal 65079
+    end
+  end
+end
