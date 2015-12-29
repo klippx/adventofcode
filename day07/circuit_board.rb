d4a4801cd3d4ed5c7c89e8d6a0babb155af5c7c2
Mathias Klippinge <mathias.klippinge@gmail.com>
2015-12-29 19:14:08 +0100

    Simplified

--- day07/circuit_board.rb
+++ day07/circuit_board.rb
@@ -10,37 +10,15 @@ module Day07
       until @resolved_wires.keys.size == @wires.size do
         @wires.each do |wire|
           begin
-            @resolved_wires[wire.name] = get_wire_output(wire) unless @resolved_wires.key? wire.name
+            @resolved_wires[wire.name] = wire.output(@resolved_wires) unless @resolved_wires.key? wire.name
           rescue CircuitError
+            # *swallow* - mmm... yummy!
           end
         end
       end
 
       @resolved_wires.keys.sort.map do |wire_name|
         "#{wire_name} -> #{@resolved_wires[wire_name]}"
-      end
-    end
-
-    def tap_wire_source(input_name)
-      input = if input_name
-        if input_name[/\d+/]
-          input_name.to_i
-        elsif @resolved_wires.key? input_name
-          @resolved_wires[input_name]
-        end
-      end
-    end
-
-    def get_wire_output(wire)
-      operator = wire.source.instance_variable_get(:@operator)
-      if operator
-        input_one_name = wire.source.instance_variable_get(:@input_one)
-        input_two_name = wire.source.instance_variable_get(:@input_two)
-        input_one = tap_wire_source(input_one_name)
-        input_two = tap_wire_source(input_two_name)
-        Gate.new(input_one, operator, input_two).output or fail CircuitError
-      else
-        wire.source.output
       end
     end
   end
--- day07/wire.rb
+++ day07/wire.rb
@@ -12,5 +12,29 @@ module Day07
     def source
       @source
     end
+
+    def tap_wire_source(input_name, resolved_wires)
+      input = if input_name
+        if input_name[/\d+/]
+          input_name.to_i
+        elsif resolved_wires.key? input_name
+          resolved_wires[input_name]
+        end
+      end
+    end
+
+    def output(resolved_wires)
+      operator = @source.instance_variable_get(:@operator)
+      if operator
+        input_one_name = @source.instance_variable_get(:@input_one)
+        input_two_name = @source.instance_variable_get(:@input_two)
+        input_one = tap_wire_source(input_one_name, resolved_wires)
+        input_two = tap_wire_source(input_two_name, resolved_wires)
+        Gate.new(input_one, operator, input_two).output or fail CircuitError
+      else
+        @source.output
+      end
+    end
+
   end
 end
