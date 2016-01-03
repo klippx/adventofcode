b30396a894468c2885531eb4538a22eca227f548
Mathias Klippinge <mathias.klippinge@gmail.com>
2016-01-03 22:41:04 +0100

    Very easy to add max distance for day 9 part 2

--- day09/parser.rb
+++ day09/parser.rb
@@ -5,16 +5,25 @@ module Day09
     end
 
     def to_s
-      "Result: #{shortest_distance}\n"
+      process
+      "Result, part1: #{shortest_distance}\n" +
+      "Result, part2: #{longest_distance}\n"
     end
 
-    def shortest_distance
+    def process
+      Location.clear
       @input.split("\n").each do |route|
         origin_name, destination_name, distance = RouteData.parse(route)
         Route.create(origin_name, destination_name, distance)
       end
+    end
 
+    def shortest_distance
       calculate_all_distances.min
+    end
+
+    def longest_distance
+      calculate_all_distances.max
     end
 
     def calculate_all_distances
--- test/day09/parser_test.rb
+++ test/day09/parser_test.rb
@@ -4,7 +4,17 @@ require './day09/day09'
 describe Day09::Parser do
   describe '#shortest_distance' do
     it 'The shortest of these is London -> Dublin -> Belfast = 605' do
-      Day09::Parser.new(File.open('./test/day09/fixtures/parser_input.txt').read).shortest_distance.must_equal 605
+      subject = Day09::Parser.new(File.open('./test/day09/fixtures/parser_input.txt').read)
+      subject.process
+      subject.shortest_distance.must_equal 605
+    end
+  end
+
+  describe '#longest_distance' do
+    it 'the longest route would be 982 via (for example) Dublin -> London -> Belfast.' do
+      subject = Day09::Parser.new(File.open('./test/day09/fixtures/parser_input.txt').read)
+      subject.process
+      subject.longest_distance.must_equal 982
     end
   end
 end
